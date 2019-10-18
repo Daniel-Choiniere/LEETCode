743f56fd0c14b49d5f0238e2ab57cf856fd91ae0
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-10-18 13:35:37 -0400

    working out logic for addTwonumbers algorithim

---
+++ addTwoNumbers.js
@@ -0,0 +1,11 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+// Example 1:  Input: 123 Output: 321
+
+var reverse = function(x) {
+  var reversed;
+  for (var i = x.length; i >= 0; i--) {
+    return x[i];
+  }
+};
+
+console.log(reverse(123));
--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,5 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
 var twoSum = function(nums, target) {
   for (var i = 0; i < nums.length; i++) {
     for (var j = i + 1; j < nums.length; j++) {
